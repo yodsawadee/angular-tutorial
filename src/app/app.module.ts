dfdb53154a2e65b5bfedd66333b2205262b47124
yodsawadee <jan.yodsawadee@outlook.com>
2024-06-27 19:11:58 +0700

    fix error lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CollapseModule } from '@coreui/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +25,6 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    CollapseModule,
     ComponentModule,
     PageModule,
     MatButtonModule,
