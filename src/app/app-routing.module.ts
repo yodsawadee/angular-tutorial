9001ab230b8c07d5f89a9d2328b2a4ba080e6cef
yodsawadee <jan.yodsawadee@outlook.com>
2024-06-28 19:26:06 +0700

    update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 
 // const routes: Routes = [];
 const routes: Routes = [
-  { path: '', redirectTo: '/portfolio', pathMatch: 'full' },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: PortfolioComponent },
   { path: 'main', component: MainComponent },
-  { path: 'portfolio', component: PortfolioComponent },
   { path: 'page', loadChildren: () => import('./page/page.module').then(m => m.PageModule) },
-  // { path: '**', component: AppComponent },  // Wildcard route for a 404 page
+  { path: '**', component: PortfolioComponent },  // Wildcard route for a 404 page
 ];
 
 @NgModule({
